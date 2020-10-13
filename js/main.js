a85848cbd47338afaf7366a5d164f02218e6bf6e
IreneRuedaVega <ruedavega.irene@gmail.com>
2020-10-13 10:04:02 +0200

    Add function checkNumber incomplete

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 'use strict';
 
-//Constantes
+//Variables
 const number = document.querySelector(".js-number");
 const btn = document.querySelector(".js-button");
+const clue = document.querySelector(".js-clue");
 
-//Función número random
+
+
+//Function to get the random number
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
@@ -12,3 +15,14 @@ function getRandomNumber(max) {
 const random = getRandomNumber(100);
 console.log(`Mi número aleatorio es ${random}`);
 
+clue.innerHTML = ' Escribe un número y dale a Prueba ';
+
+//Function to check the number
+function checkNumber(){
+  let numberValue = number.value;
+  numberValue = parseInt(numberValue);
+  
+}
+
+//event button 
+btn.addEventListener("click", checkNumber);
