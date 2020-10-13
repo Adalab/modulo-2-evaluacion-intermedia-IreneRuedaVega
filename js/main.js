c4c3786d8f39554154d54bbb613f26ade38fd21c
IreneRuedaVega <ruedavega.irene@gmail.com>
2020-10-13 09:30:44 +0200

    Add function getRandomNumber

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 'use strict';
 
-console.log("Hola mundo");
+//Constantes
+const number = document.querySelector(".js-number");
+const btn = document.querySelector(".js-button");
+
+//Función número random
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+}
+
+const random = getRandomNumber(100);
+console.log(`Mi número aleatorio es ${random}`);
 
