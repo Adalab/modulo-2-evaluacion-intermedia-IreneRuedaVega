7d5d8e9cb52b622666d7d47fe177b2d2687a95bc
IreneRuedaVega <ruedavega.irene@gmail.com>
2020-10-13 10:57:32 +0200

    Add function checkAttempts with fails

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
-'use strict';
+"use strict";
 
 //Variables
 const number = document.querySelector(".js-number");
 const btn = document.querySelector(".js-button");
 const clue = document.querySelector(".js-clue");
-const guesses = document.querySelector(".js-guess");
-
-let attempts = 0;
+const guess = document.querySelector(".js-guess");
 
 //Function to get the random number
 function getRandomNumber(max) {
@@ -16,30 +14,35 @@ function getRandomNumber(max) {
 const random = getRandomNumber(100);
 console.log(`Mi número aleatorio es ${random}`);
 
-clue.innerHTML = ' Escribe un número y dale a Prueba ';
+clue.innerHTML = " Escribe un número y dale a Prueba ";
 
 //Function to check the number
-function checkNumber(){
+function checkNumber() {
   let numberValue = number.value;
   numberValue = parseInt(numberValue);
   console.log(numberValue);
-  if(numberValue >=1 && numberValue <=100 ){
-    if(random === numberValue){
-      clue.innerHTML = ' ¡Has ganado campeona! ';
-    }else if(random < numberValue){
-      clue.innerHTML = ' Demasiado alto ';
-    }else if(random > numberValue){
-      clue.innerHTML = ' Demasiado bajo ';
+  if (numberValue >= 1 && numberValue <= 100) {
+    if (random === numberValue) {
+      clue.innerHTML = " ¡Has ganado campeona! ";
+    } else if (random < numberValue) {
+      clue.innerHTML = " Demasiado alto ";
+    } else if (random > numberValue) {
+      clue.innerHTML = " Demasiado bajo ";
     }
-  }else{
-    clue.innerHTML = ' El número debe estar entre 1 y 100 ';
+  } else {
+    clue.innerHTML = " El número debe estar entre 1 y 100 ";
   }
 }
 
+let attempts = 0;
+
 //Function to check the attempts
-function getAttempts(){
-  
+function checkAttempts() {
+  const attemptsNumber = attempts + 1;
+  guess.innerHTML = attemptsNumber;
+  /* attemptValue ++; */
 }
 
-//event button 
+//event button
 btn.addEventListener("click", checkNumber);
+btn.addEventListener("click", checkAttempts);
